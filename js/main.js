d7555bca63bca417b0a60c64c5e26318a0549fe4
Amos Marsters <amosmarsters@Amoss-MacBook-Pro.local>
2016-03-31 15:49:00 -0400

    Help

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$('.btn').on('click', function (e) {
+  $(this).removeClass('lines');
+});
+
+$('.btn').on('click', '.lines', function (e) {
+  $(this).addClass('lines');
+});
+
+$('.btn').on('click', function (e) {
+  $(this).addClass('engage');
+});
+
+
+
+
+
